ada516b13c7ebe5d4b5fcbc32c431f5b3262d77b
Daniel Muñoz <dmc145@gmail.com>
2013-10-03 12:55:02 +0200

    BNR iOs Chap. 4

---
+++ Whereami/Whereami/WhereamiViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhereamiViewController.h
+//  Whereami
+//
+//  Created by Daniel Muñoz on 30/09/13.
+//  Copyright (c) 2013 Daniel Muñoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiViewController : UIViewController
+
+@end
---
+++ Whereami/Whereami/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Whereami
+//
+//  Created by Daniel Muñoz on 30/09/13.
+//  Copyright (c) 2013 Daniel Muñoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WhereamiAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WhereamiAppDelegate class]));
+    }
+}
